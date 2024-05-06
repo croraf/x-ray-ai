4a46389dec56caeea8266773683ed46c81639d77
croraf <vrafaeli@msn.com>
2024-05-06 13:40:21 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,60 @@
+import argparse from "argparse";
+
+import data from "./data";
+import { model } from "./model";
+
+async function run(epochs, batchSize, modelSavePath) {
+  await data.loadData();
+
+  const { images: trainImages, labels: trainLabels } = data.getTrainData();
+  model.summary();
+
+  let epochBeginTime;
+  let millisPerStep;
+  const validationSplit = 0.15;
+  const numTrainExamplesPerEpoch = trainImages.shape[0] * (1 - validationSplit);
+  const numTrainBatchesPerEpoch = Math.ceil(
+    numTrainExamplesPerEpoch / batchSize,
+  );
+  await model.fit(trainImages, trainLabels, {
+    epochs,
+    batchSize,
+    validationSplit,
+  });
+
+  const { images: testImages, labels: testLabels } = data.getTestData();
+  const evalOutput = model.evaluate(testImages, testLabels);
+
+  console.log(
+    `\nEvaluation result:\n` +
+      `  Loss = ${evalOutput[0].dataSync()[0].toFixed(3)}; ` +
+      `Accuracy = ${evalOutput[1].dataSync()[0].toFixed(3)}`,
+  );
+
+  if (modelSavePath != null) {
+    await model.save(`file://${modelSavePath}`);
+    console.log(`Saved model to path: ${modelSavePath}`);
+  }
+}
+
+const parser = new argparse.ArgumentParser({
+  description: "TensorFlow.js-Node MNIST Example.",
+  add_help: true,
+});
+parser.add_argument("--epochs", {
+  type: "int",
+  default: 20,
+  help: "Number of epochs to train the model for.",
+});
+parser.add_argument("--batch_size", {
+  type: "int",
+  default: 128,
+  help: "Batch size to be used during model training.",
+});
+parser.add_argument("--model_save_path", {
+  type: "string",
+  help: "Path to which the model will be saved after training.",
+});
+const args = parser.parse_args();
+
+run(args.epochs, args.batch_size, args.model_save_path);
