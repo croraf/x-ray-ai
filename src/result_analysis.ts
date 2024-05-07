cec671e8134a96d9c8d580e895c89c3dae311854
croraf <vrafaeli@msn.com>
2024-05-07 11:59:10 +0200

    improve result analysis

--- src/result_analysis.ts
+++ src/result_analysis.ts
@@ -7,12 +7,13 @@ export const resultAnalysis = async (
 ) => {
   const predictions = model.predict(data) as tf.Tensor<tf.Rank.R2>;
   predictions.print();
-  /* const predictionsData = predictions.dataSync();
 
+  const predictionsData = predictions.dataSync();
+
+  // summary
   const predictionsInt: number[] = [];
   for (let i = 0; i < predictions.shape[0]; i++) {
     for (let j = 0; j < predictions.shape[1]; j++) {
-      console.log(predictionsData[i * predictions.shape[1] + j]);
       if (predictionsData[i * predictions.shape[1] + j] === 1) {
         predictionsInt.push(j);
       }
@@ -24,5 +25,5 @@ export const resultAnalysis = async (
   const wrongEvaluations = predictionsInt.map((prediction, index) =>
     prediction - labelsPlain[index] === 0 ? " " : labelsPlain[index].toString(),
   );
-  console.log(wrongEvaluations); */
+  console.log(wrongEvaluations);
 };
